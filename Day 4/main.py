e3e03af808757c605fdf1939472515f3c6ffd951
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-01 23:53:49 +0500

    Day 4

---
+++ Day 4/main.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+print("My Poem " \
+        "is a beautiful thing."
+         "It brings joy to my heart." \
+            "I love to write it every day." \
+                "It makes me feel alive.")
+
+print("My Poem:\n" 
+      "roses are red, "
+      "violets are blue, "
+      "I love to write, "
+      "and so do you."
+      )
