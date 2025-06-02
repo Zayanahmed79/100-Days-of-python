5d925d0c067b742987120d06d3eb6d4202c65c2a
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-02 16:52:48 +0500

    Day 10

---
+++ Day 10/main.py
@@ -0,0 +1,9 @@
+a = input("ENter your name: ")
+print("Hello " + a + "! Welcome to the Python world.")
+
+
+
+b = input("Enter First Number:") # This will take input as string that is why we need to convert it to int
+c = input("Enter Second Number:")
+
+print("The Sum of number is" , int(b) + int(c))
