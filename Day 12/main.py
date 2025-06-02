0237278e7bc1df620c52d5cf5313915ca7d0872b
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-02 18:57:26 +0500

    Day 12

---
+++ Day 12/main.py
@@ -0,0 +1,19 @@
+name =  "Zayan_kz"
+len = len(name)
+print("In Zayan_kz, there are" , len , "characters")
+
+
+#slicing 
+
+fruit = "Apple"
+print(fruit[0:3]) # including 0, excluding 3
+print(fruit[1:4]) # including 1, excluding 4
+
+# negative indexing 
+
+print(fruit[-5:-2]) # including -5, excluding -2    (in negative indexing the negavtive number are cut from the orignal length of the string)
+
+
+nm = "harry"
+print(nm[-4:-2]) # including -4, excluding -2
+
