b2a141745f2a7a1a29a0fb3cc0405f348643e294
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-03 23:34:23 +0500

    Day 17

---
+++ Day 17/main.py
@@ -0,0 +1,17 @@
+# For  loop 
+
+names = ["Zayan", "Zain", "Zeeshan", "Zainab", "Zara"]
+# for name in names:
+#     print(name)
+#     for letter in name:
+#         print(letter)
+
+
+# For loop with range
+
+for i in range(5, 10, 2) : # start, stop, step are used to define the range
+    print(i)
+    #output:
+    # 5
+    # 7
+    # 9
