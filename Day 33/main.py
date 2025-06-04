728cdee4deeb924ce4bf437d83fed29833143ab7
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-04 21:37:31 +0500

    Day 33

---
+++ Day 33/main.py
@@ -0,0 +1,25 @@
+dic1 = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+print(dic1["name"])
+
+
+dic3 = {
+    "303" : "Zayan",
+    "404" : "ali",
+    "505" : "Ahmed"
+}
+
+print(dic3["404"])
+dic3["404"] = "Ali"
+print(dic3["404"])
+
+dic3["606"] = "Hassan"
+print(dic3["606"])
+
+dic3["707"] = "Hassan"
+print(dic3)
+
