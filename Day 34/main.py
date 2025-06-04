3a98440299416801acdc7c527d5bbedc9e87d0e8
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-04 22:36:17 +0500

    Day 34

---
+++ Day 34/main.py
@@ -0,0 +1,15 @@
+# This code defines two dictionaries representing the scores of students in two different episodes.
+ep2 = {"Zayan": 303, "Ali": 404, "Ahmed": 505, "Hassan": 606}
+ep3 = {"Sara": 707, "Omar": 808, "Lina": 909}
+# ep2.update(ep3) 
+# ep2.pop("Zayan")  # This will remove the key "Zayan" from ep2
+# print(ep2)
+
+# ep2.popitem()
+# print(ep2)
+
+# ep2.clear()  # This will remove all items from ep2
+# print(ep2)
+
+# del ep2
+# print(ep2)  # This will raise an error since ep2 has been deleted
