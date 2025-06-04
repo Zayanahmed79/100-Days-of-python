54ff385256ee653531deaae4e33f6e2fe720cfbe
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-04 21:44:45 +0500

    Day 33

--- Day 33/main.py
+++ Day 33/main.py
@@ -20,6 +20,18 @@ print(dic3["404"])
 dic3["606"] = "Hassan"
 print(dic3["606"])
 
-dic3["707"] = "Hassan"
-print(dic3)
+# print(dic3["707"]) # This will through an error if item not found
+# print(dic3.get("707"))  # This will return "Not Found" since key "707" does not exist
+
+print(dic3.keys())  # This will return a view object that displays a list of all the keys in the dictionary
+print(dic3.values())  # This will return a view object that displays a list of all the values in the dictionary
+
+
+for key in dic3.keys():
+    print(key)  # This will print each key in the dictionary
+
+for key, value in dic3.items():
+    print(f"{key}: {value}")  # This will print each key-value pair in the dictionary
+
+
 
