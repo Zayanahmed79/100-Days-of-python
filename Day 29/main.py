7416f7462e055ff1f93b6a11b9c17e78faf40cc1
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-04 17:58:49 +0500

    Day 29

---
+++ Day 29/main.py
@@ -0,0 +1,14 @@
+def sum(a , b):
+    """
+    This Fuvtion  takes two numbers and returns their sum.
+    """
+    return a + b
+
+print(sum(4,6))
+
+print(sum.__doc__)
+
+print(sum.__sizeof__()) # This code prints the size of the function object in bytes.
+
+# This code defines a function `sum` that takes two numbers as input and returns their sum.
+# The function also includes a docstring that describes its purpose.
