b2d5d3796f51a1ab5281f9fbc540d2b1723e376e
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-04 17:11:13 +0500

    Day 25

---
+++ Day 25/main.py
@@ -0,0 +1,13 @@
+tuple = (1, 3 ,5 , 7, 9, 3, 5, 7, 9)
+
+tuple2 = tuple.count(3)  # Count occurrences of 3 in the tuple
+print("Count of 3 in tuple:", tuple2)
+
+tuple3 = tuple.index(5 )  # Find the index of the first occurrence of 5
+print("Index of first occurrence of 5 in tuple:", tuple3)
+
+tuple4 = tuple.index(5, 3)  # Find the index of the first occurrence of 5 starting from index 3
+print("Index of first occurrence of 5 starting from index 3:", tuple4)
+
+
+
