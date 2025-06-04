f63c0ed409c83502afbd6da49f8db175b6a97dc7
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-04 17:42:32 +0500

    Day 28

---
+++ Day 28/main.py
@@ -0,0 +1,9 @@
+name = "Zayan Ahmed"
+country = "Pakistan"
+
+print(f"Hello, {name} from {country}!")
+# This code prints a greeting message that includes the user's name and country.
+
+price = 49.98989
+print(f"The price is: ${price:.2f}")
+# This code formats the price to two decimal places, ensuring it is displayed as a monetary value.
