69fa578f3a896b93315e13ee3e5c159091cb730f
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-05 02:51:18 +0500

    Day 42

---
+++ Day 42/main.py
@@ -0,0 +1,7 @@
+marks  = [12 , 15, 14, 10, 18, 20, 16, 19, 17, 13]
+
+for index, mark in enumerate(marks):
+    print(index, mark)
+    if mark == 15:
+        print("Found 15, So Zayan is a good boy")
+
