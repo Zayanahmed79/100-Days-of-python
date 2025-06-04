9bab146563148eea558076d3fb8f213909e61c5b
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-04 12:57:51 +0500

    Day 22

--- Day 22/main.py
+++ Day 22/main.py
@@ -29,3 +29,19 @@ print(l[::2])  # [1, 7, 12] , every second element
 print(l[1::2])  # [4, 9] , starting from index 1, every second element
 print(l[::-1])  # [12, 9, 7, 4, 1] , reversed list 
 
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[:6])	#using positive indexes
+print(animals[:-3])	#using negative indexes
+
+# List comprehensions
+list = [i for i in range(1, 11) ]
+print(list)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# List comprehensions with calculations
+list = [i*i for i in range(1, 11) ]
+print(list)  # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# List comprehensions with conditions
+list = [i for i in range(1, 11) if i % 2 == 0]
+print(list)  # [2, 4, 6, 8, 10]
+
+
