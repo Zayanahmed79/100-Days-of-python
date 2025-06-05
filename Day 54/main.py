07913ef94bc25c86ec50c5ea98a472878ffc544a
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-06 00:47:35 +0500

    Day 54

---
+++ Day 54/main.py
@@ -0,0 +1,20 @@
+a = 12
+b = "12"
+
+print(a == b) # False  , checjs if exact same object in memory
+print(a is b) # False  ,  cheks only tthe value
+
+c = 12
+b = 12
+print(a == b) # true
+print(a is b) # true
+
+
+f = [1 , 2, 3]
+g = [1 , 2, 3]
+print(f == g) # true cheks if exact same object in memory
+print(f is g) # False cheks only tthe value
+
+
+
+
